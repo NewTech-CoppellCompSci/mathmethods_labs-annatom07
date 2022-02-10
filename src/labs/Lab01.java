dd9c7256e95e22f05a590f84f441e05f6a267b66
annatom07 <atom@newtech.coppellisd.com>
2022-02-10 17:34:36 -0600

    Lab 1

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -1,6 +1,6 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab01 {
 
@@ -16,17 +16,27 @@ public class Lab01 {
 
 	
 	public static void problem01() {
+		Scanner inKey = new Scanner(System.in);// activating the scanner 
+		int entnum;
+		System.out.println("Put a positive integer");
+		entnum = inKey.nextInt();
+		int entnum1;
+		System.out.println("Enter another positve integer");
+		entnum1 = inKey.nextInt();
 		
-		
+		double num1Pow = Math.pow(entnum,entnum1);//return type = double
+		System.out.println(num1Pow + "\n");
 		
 	}
-	
-	
-	
-	
+
 	public static void problem02() {
-		
-		
+		Scanner inKey = new Scanner(System.in);// activating the scanner 
+		int entnum;
+		System.out.println("Put a positive integer");
+		entnum = inKey.nextInt();
+		double num2 = Math.sqrt(entnum);//The sqrt is going to find the square  root
+		System.out.println(num2 + "\n");
+
 		
 	}
 
@@ -34,9 +44,14 @@ public class Lab01 {
 	
 
 	public static void problem03() {
-		
-		
-		
+		Scanner inKey = new Scanner(System.in);
+		double entA, entB, hypotenuse;   
+		System.out.println("Length of  side A");//Enter a number 
+		entA = inKey.nextInt();
+		System.out.println("Length of  side B");//Enter another number 
+		entB = inKey.nextInt();
+		hypotenuse = Math.sqrt((entA*entA)+(entB*entB));
+		System.out.println("Hypotenuse = " + hypotenuse);
 
 	}
 
@@ -44,9 +59,30 @@ public class Lab01 {
 	
 	
 	public static void problem04() {
+		Scanner inKey = new Scanner(System.in);// activating the scanner 
+		int entnum;
+		int max = Integer.MIN_VALUE;//Max value
+		int min = Integer.MAX_VALUE;//Min value
 		
+		System.out.print("Enter an integer");
+		entnum = inKey.nextInt();
+		while (entnum > 0)//While 
+			{
+		System.out.print("Enter an integer");
+		entnum = inKey.nextInt();
+		if (entnum < 0)//if
+		{
+		System.out.print("Enter an integer");
+		entnum = inKey.nextInt();
+		max = Math.max(max, entnum);
 		
-		
+		System.out.println(max);
+		System.out.println("Max =" + max);
+		min = Math.min(min, entnum);
+		System.out.println(min);
+		System.out.println("Min =" + min);
+		}
+	}
 	}
 	
 	
